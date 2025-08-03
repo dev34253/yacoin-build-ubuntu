2f9170f305f51b69c8a07b557bb0f724a41f6390
dev34253 <dev34253@gmail.com>
2025-08-03 16:39:55 -0400

    build: prep docker image for depends version of build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "========================= Building yacoin"
+echo "== Building yacoin =="
 echo "Running in folder:"
 pwd
 
-./autogen.sh
-./configure
-make
-mkdir debug
+# for genbuild.sh to run without errors on Ubuntu 20
+git config --global --add safe.directory /src
 
-make release
+cd depends/
+make -j 4 HOST=x86_64-pc-linux-gnu
+
+cd ..
+./autogen.sh
+CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure --enable-glibc-back-compat --enable-reduce-exports LDFLAGS=-static-libstdc++ --prefix=/
+make -j 4
+make install DESTDIR=`pwd`/release-root
