d2c2a28a97288f2c75ca40c97456deb92db8a2dc
dev34253 <dev34253@gmail.com>
2025-08-03 20:46:54 -0400

    some debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,20 @@ pwd
 # for genbuild.sh to run without errors on Ubuntu 20
 git config --global --add safe.directory /src
 
+echo "== Building dependencies =="
 cd depends/
+echo "Running in folder:"
+pwd
+echo "Content:"
+ls -la
 make -j 4 HOST=x86_64-pc-linux-gnu
 
+echo "== Building yacoin =="
 cd ..
+echo "Running in folder:"
+pwd
+echo "Content:"
+ls -la
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-pc-linux-gnu/share/config.site ./configure --enable-glibc-back-compat --enable-reduce-exports LDFLAGS=-static-libstdc++ --prefix=/
 make -j 4
